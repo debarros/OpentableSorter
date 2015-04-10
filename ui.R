0dad183691e4e9cfd6e5f396bb14a09f950dabe2
pj.debarros@gmail.com <pj.debarros@gmail.com>
2015-04-10 15:25:04 -0400

    initial commit of user interface file

---
+++ ui.R
@@ -0,0 +1,21 @@
+library(shiny)
+
+shinyUI(fluidPage(
+  titlePanel("Sort Through OpenTable Brunch Options"),
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Find a listing of Sunday Brunch options from open table.  Copy the web address and paste it below."),
+      textInput(
+        inputId = "url",
+        label = "OpenTable Web Address", 
+        value = "paste the address here"),
+      helpText("After the list of checkboxes loads, follow then directions and then move to the next tab.")
+    ), #end of sidebarPanel
+    mainPanel(tabsetPanel(
+      tabPanel("Locations", uiOutput("Locations")),
+      tabPanel("Fare", uiOutput("Fares")),
+      tabPanel("Beverages", uiOutput("Beverages")),
+      tabPanel("Results", uiOutput("Results"))
+    )) #end of mainPanel
+  ) #end of sidebarLayout
+)) #end of shinyUI
